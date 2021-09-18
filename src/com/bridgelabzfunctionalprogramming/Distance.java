e2c9944fb1c9c29326449220514f30b02b6dba6e
777shetty <shetty.chetu7@gmail.com>
2021-09-18 11:31:42 +0530

    ADDED:EcludianDistance

---
+++ src/com/bridgelabzfunctionalprogramming/Distance.java
@@ -0,0 +1,10 @@
+package com.bridgelabzfunctionalprogramming;
+
+public class Distance {
+	public static void main(String[] args) {
+		int x = Integer.parseInt(args[0]), y = Integer.parseInt(args[1]);
+		System.out.println("Euclidean distance: "+Math.sqrt(x*x+y*y));
+	}
+	}
+
+
